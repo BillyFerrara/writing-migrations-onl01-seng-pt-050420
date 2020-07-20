9800459eddea2b26c4103083d658f599b3989415
Billy Ferrara <willfira717@gmail.com>
2020-07-20 18:03:10 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,9 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 
   def change
-    change_column(table_name, column_name, type)
+    change_column(table_name, column_name, type) :students do |t|
+      t.datetime :birthdate
+    end 
   end
 
 end
